d124f26fb759fb7c873492d22a1748f1967812e7
Aminaaliyu2 <amina2fit@gmail.com>
2023-05-22 15:01:07 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - A program that print a line using the printf function
  *
- * Return: always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
-}
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}		
