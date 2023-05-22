66cb6c835da6398c57fcd72d9a14c0c02643aa82
Aminaaliyu2 <amina2fit@gmail.com>
2023-05-22 13:58:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - A  program that print a line using the printf function
+ * main - A program that print a line using the printf function
  *
- *Return: 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
